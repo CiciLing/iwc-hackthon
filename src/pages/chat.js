20b9eeeb3f95a1fdf45aff2f5d9ce64a817330d2
CiciLing <cicilingg0502@gmail.com>
2022-11-19 15:07:15 -0500

    navbar

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,14 +1,14 @@
 // commit
 import React from 'react';
-import { Nav, NavLink, NavMenu }
-    from "./NavBarelements";
+import './chat.css';
 
 const Chat = () => {
   return (
     <div className="chatsinforum">
-      <h1>Community and Forum</h1>
-      <h1>it still work?</h1>
-      <h2>yes</h2>
+      <h1>Forum</h1>
+      <h2>FAQ</h2>
+      <h2>Submit Your Question!</h2>
+
 
 
     </div>

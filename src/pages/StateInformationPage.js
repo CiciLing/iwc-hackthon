b9c9aa5896f20840ee351f1d4cbb47cfaee04fff
ananyar807 <ananyar807@gmail.com>
2022-11-19 14:25:10 -0500

    state info

--- src/pages/StateInformationPage.js
+++ src/pages/StateInformationPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import StateInformation from './StateInformation.js'
+import StateInformation from '../StateInformation.js'
   
 const StateInformationPage = () => {
   return (
